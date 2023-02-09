4fc3f1acbf3269b17b880a5478d26578569a6311
Omar Bognanno <omarbognanno@microsoft.com>
2023-02-09 18:40:30 +0000

    Yay

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,144 @@ import logo from './logo.svg';
 import './App.css';
 import { AppCard } from './components/AppCard';
 
+function AppSectionTitle({name}:{name:string}) {
+  return <h1 style={{
+    display: "flex",
+    justifyContent: "space-around",
+    color: "white"
+  }}>
+    {name}
+  </h1>;
+}
+
+function AppCardGrid({cards}:{cards:any[]}) {
+  return <div style={{
+    padding: "1em",
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "1em"
+  }}>
+    {cards}
+  </div>
+}
+
+function AppCardSection({name, cards}: {name:string, cards:any[]}) {
+  return <div>
+    <AppSectionTitle name={name}/>
+    <AppCardGrid cards={cards}/>
+  </div>
+}
+
 function App() {
   return (
     <div className="App">
-      <div style={{padding: "1em"}}>
-        <AppCard></AppCard>
+      {/* BODY ROOT */}
+      <div style={{
+        padding: "1em",
+      }}>
+        <AppCardSection
+          name="Scavenger Hunts"
+          cards={[
+            
+            <AppCard name="Kimchi Connoisseur" category="Scavenger Hunt"
+              imageText="🌶️"
+              bodyMain="Try 30 types of Kimchi"
+              flavourText="There is a bunch in the Kimchikan Museum."/>,
+            
+            <AppCard name="Soju Connoisseur" category="Scavenger Hunt"
+              imageText="🍶"
+              bodyMain="Try 30 types of Soju"
+              flavourText="Geonbae."/>,
+            
+            <AppCard name="Street Food Connoisseur" category="Scavenger Hunt"
+              imageText="🥡"
+              bodyMain="Try 30 types of Street Food"
+              flavourText="There is a bunch in Myeongdong Shopping Street."/>,
+            
+            <AppCard name="Safari" category="Scavenger Hunt"
+              imageText="📸"
+              bodyMain="Try 30 types of Street Food"
+              flavourText="Take pictures of"/>,
+          ]}
+        />
+
+<AppCardSection
+          name="Experiences"
+          cards={[
+            
+            <AppCard name="Karaoke Show" category="Experience"
+              imageText="🎤"
+              bodyMain="Sing a whole song from a famous K-Pop band"
+              flavourText="YE YE YE."/>,
+            
+            <AppCard name="Soju Connoisseur" category="Experience"
+              imageText="🍶"
+              bodyMain="Try 30 types of Soju"
+              flavourText="Geonbae."/>,
+            
+            <AppCard name="Idol Dance" category="Experience"
+              imageText="💃🏽"
+              bodyMain="Perform the choreography from a famous K-Pop song"
+              flavourText="It can be practiced anywhere, but the performance will happen in the busy streets of Seoul."/>,
+            
+            <AppCard name="Taekwon-do" category="Experience"
+              imageText="🥋"
+              bodyMain="Attend to a lesson of Taekwon-do"
+              flavourText="And break a piece of wood."/>,
+
+            <AppCard name="The Five Palaces" category="Experience"
+              imageText="🏯"
+              bodyMain="Visit the “Five palaces” wearing a hanbok, learn about Korea’s history and test her knowledge with a quiz."
+              flavourText=""/>,
+
+            <AppCard name="K-Drama" category="Experience"
+              imageText="🎭"
+              bodyMain="Perform a scene from a K-Drama"
+              flavourText="Three options will be presented"/>,
+
+            <AppCard name="Hangul" category="Experience"
+              imageText="한글"
+              bodyMain="Write something using the Korean Ideograms"
+              flavourText=""/>,
+
+            <AppCard name="Buddha Something" category="Experience"
+              imageText="🪷"
+              bodyMain="Dunno but temple."
+              flavourText="Jogyesa Buddhist Temple or Jongmyo Shrine."/>,
+
+            <AppCard name="Cable Car" category="Experience"
+              imageText="🚠"
+              bodyMain="No clue, the view is nice at night."
+              flavourText="Namsan Cable Car"/>,
+
+            <AppCard name="Cruise" category="Experience"
+              imageText="🚢"
+              bodyMain="Dunno."
+              flavourText="Eland Cruise"/>,
+          ]}
+        />
+
+
+        <AppCardSection
+          name="Modifiers"
+          cards={[
+            
+            <AppCard name="Korean conversation" category="Modifier"
+              imageText="🗣️"
+              bodyMain="Order every meal in Korean and without using gestures"
+              flavourText=""/>,
+            
+            <AppCard name="12-step skincare procedure" category="Modifier"
+              imageText="🧖🏽‍♀️"
+              bodyMain="Every morning, perform the 12-step skincare procedure"
+              flavourText=""/>,
+            
+            <AppCard name="Honorifics" category="Modifier"
+              imageText="🙏🏽"
+              bodyMain="When addressing the other party members, use the typical Korean honorifics."
+              flavourText=""/>,
+          ]}
+        />
       </div>
     </div>
   );
--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -1,40 +1,118 @@
+function LightenDarkenColor(col:string, amt:number) {
+  
+  var usePound = false;
 
-export const AppCard = function () {
+  if (col[0] === "#") {
+      col = col.slice(1);
+      usePound = true;
+  }
+
+  var num = parseInt(col,16);
+
+  var r = (num >> 16);
+  var g = (num & 0x0000FF);
+  var b = ((num >> 8) & 0x00FF);
+
+  r += (255 - r) * amt;
+  g += (255 - g) * amt;
+  b += (255 - b) * amt;
+
+  if (r > 255) r = 255;
+  else if  (r < 0) r = 0;
+  
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  if (b > 255) b = 255;
+  else if  (b < 0) b = 0;
+
+  return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
+
+export const AppCard = function ({name, imageText, imageTop="-.2rem", category, bodyMain, flavourText}:any) {
+
+  const categoryColors: any = {
+    "Scavenger Hunt": "#b20d30",
+    "Experience": "#3F84E5",
+    "Modifier": "#c17817"
+  }
+
+  const vAlignStyle:any = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 
   return <div style={{
-    backgroundColor: "red",
+    backgroundColor: "white",
     height: "14em",
     width: "10em",
     boxSizing: "border-box",
+    borderColor: "white",
     borderStyle: "solid",
     borderRadius: ".5em",
     borderWidth: ".5em"
   }}>
     <div style={{
       height: "10%",
+      fontSize: "11px",
+      fontWeight: "bold",
+      ...vAlignStyle
     }}>
-      MY TITLE
+      <span>{name}</span>
     </div>
     
     <div style={{
       height: "40%",
-      backgroundColor: "green"
+      // backgroundColor: "lightBlue",
+      borderRadius: ".5em",
+      ...vAlignStyle
     }}>
-      MY PIC
+      <span style={{
+        fontSize: "3em",
+        position: "relative",
+        // left: 0,
+        top: imageTop //"-.1em"
+      }}>
+        {imageText}
+      </span>
     </div>
 
     <div style={{
       height: "10%",
-      backgroundColor: "magenta"
+      backgroundColor: categoryColors[category],
+      color: "white",
+      borderTopLeftRadius: ".5rem",
+      borderTopRightRadius: ".5rem",
+      fontSize: "10px",
+      ...vAlignStyle
     }}>
-      MY SUB
+      {category}
     </div>
 
     <div style={{
       height: "40%",
-      backgroundColor: "yellow"
+      // backgroundColor: "#E9E9E9",
+      backgroundColor: LightenDarkenColor(categoryColors[category], .7),
+      // filter: "brightness(0.85)",
+      fontSize: "8px",
+      padding: "1em",
+      boxSizing: "border-box",
+      borderBottomLeftRadius: ".5rem",
+      borderBottomRightRadius: ".5rem",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      // justifyContent: "space-between"
+      justifyContent: "center"
     }}>
-      MY BODY
+      <span>{bodyMain}</span>
+      {flavourText && <>
+        <br></br>
+        <span><i>{flavourText}</i></span>
+      </>}
     </div>
 
   </div>
